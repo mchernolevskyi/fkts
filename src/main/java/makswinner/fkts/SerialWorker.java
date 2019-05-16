15ada9a251f22dabcd3d18a0217b584f9824288c
Dr Maksym Chernolevskyi <maksym.chernolevskyi@gmail.com>
2019-05-16 22:36:35 +0300

    Add deflate

--- src/main/java/makswinner/fkts/SerialWorker.java
+++ src/main/java/makswinner/fkts/SerialWorker.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.util.zip.Deflater;
 
 public class SerialWorker {
 
@@ -25,15 +26,25 @@ public class SerialWorker {
         //byte b = ins.read();
 
         BufferedOutputStream out = IOUtils.buffer(serial.getOutputStream());
-        byte [] bytes = "<1234567890123456>".getBytes();
+        byte [] bytes = (
+                "The java.util.zip.Deflater.deflate(byte[] b) method https://www.tutorialspoint.com/javazip/javazip_deflater_deflate.htm T&(^&(*^&(U*(_"
+                ).getBytes();
+
+        System.out.println("Input size [" + bytes.length + "] bytes");
+        Deflater deflater = new Deflater();
+        deflater.setInput(bytes);
+        deflater.finish();
+        byte[] compressed = new byte[1024];
+        int compressedDataLength = deflater.deflate(compressed);
+        System.out.println("Compressed size [" + compressedDataLength + "] bytes");
 
         //LocalDate start = LocalDate.now();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             long start = System.currentTimeMillis();
-            out.write(bytes);
+            out.write(compressed, 0, compressedDataLength);
             out.flush();
-            System.out.println("Sent [" + bytes.length + "] bytes in [" + (System.currentTimeMillis() - start) + "] ms");
-            Thread.sleep(20000);
+            System.out.println("Sent [" + compressedDataLength + "] bytes in [" + (System.currentTimeMillis() - start) + "] ms");
+            Thread.sleep(1000);
         }
 
         serial.disconnect();
